4006d52bfa1ad3ad7d6409c3c676e58922979b2a
spotter1006 <144616792+spotter1006@users.noreply.github.com>
2024-05-06 08:38:36 -0400

    Refactor-remove BessAnalyzerService

--- src/main/java/com/eac/arbitrage/controller/AnalysisController.java
+++ src/main/java/com/eac/arbitrage/controller/AnalysisController.java
@@ -1,25 +1,20 @@
 package com.eac.arbitrage.controller;
 
 import com.eac.arbitrage.model.Analysis;
-import com.eac.arbitrage.model.Lmp;
 import com.eac.arbitrage.service.AnalysisService;
-import com.eac.arbitrage.service.BessAnalyzerService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.Instant;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/analysis")
 public class AnalysisController {
     private final AnalysisService analysisService;
-    private final BessAnalyzerService bessAnalyzerService;
+
     @Autowired
-    AnalysisController(AnalysisService analysisService, BessAnalyzerService bessAnalyzerService){
+    AnalysisController(AnalysisService analysisService){
         this.analysisService=analysisService;
-        this.bessAnalyzerService = bessAnalyzerService;
     }
 
     @GetMapping
@@ -43,7 +38,7 @@ public class AnalysisController {
 
             Analysis analysis = analysisService.getById(analysisId);
             result = "Starting analysis " + analysisId + ". check later for results ";
-            bessAnalyzerService.startAnalysis(new AnalysisDTO(analysis));
+            analysisService.startAnalysis(new AnalysisDTO(analysis));
         }catch(EntityNotFoundException e){
             result = "Analysis with ID " + analysisId + " not found";
         }
--- src/main/java/com/eac/arbitrage/service/AnalysisService.java
+++ src/main/java/com/eac/arbitrage/service/AnalysisService.java
@@ -3,16 +3,25 @@ package com.eac.arbitrage.service;
 import com.eac.arbitrage.model.Analysis;
 import com.eac.arbitrage.controller.AnalysisDTO;
 import com.eac.arbitrage.repository.AnalysisRepository;
+import com.eac.arbitrage.repository.LmpRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Service
 public class AnalysisService {
     private final AnalysisRepository analysisRepository;
+    private final ExecutorService executorService;
+    private final LmpRepository lmpRepository;
+
     @Autowired
-    AnalysisService(AnalysisRepository analysisRepository){
+    AnalysisService(AnalysisRepository analysisRepository, LmpRepository lmpRepository){
         this.analysisRepository=analysisRepository;
+        this.executorService = Executors.newSingleThreadExecutor();
+        this.lmpRepository = lmpRepository;
     }
     public Analysis getById(Long id){
         return analysisRepository.getReferenceById(id);
@@ -23,5 +32,8 @@ public class AnalysisService {
     public Analysis addAnalysis(Analysis analysis){
         analysisRepository.save(analysis);
         return(analysis);
+    }
+    public void startAnalysis(AnalysisDTO analysis){
+        executorService.submit(new AnalyzeRunnable(analysis, lmpRepository));
     }
 }
